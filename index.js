da0ee10bd3c2eb77ff9e7e32dc1716b7f3253a32
Mai <81833600+maingo-png@users.noreply.github.com>
2021-09-30 15:18:17 -0400

    update 9/30

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-console.log("Your index.js file is loaded correclty!");
+/*Home*/ 
+
+$(".Home").mouseenter(function() {
+    $('#homeTxt').css('overflow','visible');
+    
+ });
+
+ $(".Home").mouseleave(function() {
+    $('#homeTxt').css('overflow','hidden');
+ });
+
+ /*Work*/ 
+
+ $(".Work").mouseenter(function() {
+    $('#workTxt').css('overflow','visible');
+
+ 
+ });
+ 
+ $(".Work").mouseleave(function() {
+     $('#workTxt').css('overflow','hidden');
+   
+  });
+
+/*About*/ 
+
+ $(".About").mouseenter(function() {
+    $('#aboutTxt').css('overflow','visible');
+ 
+    
+ });
+
+ $(".About").mouseleave(function() {
+    $('#aboutTxt').css('overflow','hidden');
+ 
+ });
+
+/*Contact*/
+
+ $(".Contact").mouseenter(function() {
+    $('#contactTxt').css('overflow','visible');
+ 
+    
+ });
+
+ $(".Contact").mouseleave(function() {
+    $('#contactTxt').css('overflow','hidden');
+
+ });
